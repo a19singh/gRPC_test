48cf1636e3e60a636dbc76acbbd4e2652054bf4d
ayedaemon <ris3234@gmail.com>
2020-11-30 18:40:45 +0530

    little corrections

--- Array Details/Server.py
+++ Array Details/Server.py
@@ -5,17 +5,21 @@ import Details
 from concurrent import futures
 import time
 
+host = "127.0.0.1"
+port = "12345"
+addr = host+':'+port
+
 class ArrayDetailsServicer(details_pb2_grpc.ArrayDetailsServicer):
     def Personal(self, request, context):
         response = details_pb2_grpc.ArrayDetails()
         response.details = Details.Entry(request.Name, request.age)
-        return response
+        yield response
 
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
 details_pb2_grpc.add_ArrayDetailsServicer_to_server(ArrayDetailsServicer(), server)
 print('Starting server, Listening at port 12345')
-server.add_insecure_port('[::]:12345')
+server.add_insecure_port(addr)
 server.start()
 
 try:
@@ -24,4 +28,3 @@ try:
 except KeyboardInterrupt:
     print('Keyboard Interrupt')
     server.stop(0)
-
